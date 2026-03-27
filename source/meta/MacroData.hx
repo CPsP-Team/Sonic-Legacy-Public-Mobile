6ee6a857981d3c4123049930f4239754e7cd79e2
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2026-03-27 14:13:16 +0200

    Oop

--- source/meta/MacroData.hx
+++ source/meta/MacroData.hx
@@ -4,7 +4,7 @@ class MacroData
 {
   #if desktop
     public static macro function getDate()
-        return macro  $v{Date.now().toString();
+        return macro  $v{Date.now().toString()};
 
     public static macro function getUSR()
         return macro  $v{#if windows Sys.environment()["USERNAME"] #elseif (linux || macos) return Sys.environment()["USER"] #end};
