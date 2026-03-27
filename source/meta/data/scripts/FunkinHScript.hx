0fafc8a38a2109617b36111624e5aad95ea1d01f
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2026-03-27 14:45:38 +0200

    oh

--- source/meta/data/scripts/FunkinHScript.hx
+++ source/meta/data/scripts/FunkinHScript.hx
@@ -258,8 +258,8 @@ class FunkinHScript extends FunkinScript
 
 			try{				
 				runtime = new FlxRuntimeShader(
-					fragFile==null ? null : Paths.getContent(Paths.exists(Paths.shaderFragment(fragFile));
-					vertFile==null ? null : Paths.getContent(Paths.exists(Paths.shaderVertex(vertFile)));
+					fragFile==null ? null : Paths.getContent(Paths.exists(Paths.shaderFragment(fragFile))),
+					vertFile==null ? null : Paths.getContent(Paths.exists(Paths.shaderVertex(vertFile))));
 			}catch(e:Dynamic){
 				trace("Shader compilation error:" + e.message);
 			}
