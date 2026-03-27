32b11570f2c945f62d0f165063861841d6d49811
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2026-03-27 14:23:11 +0200

    update source/meta/MacroData.hx

--- source/meta/MacroData.hx
+++ source/meta/MacroData.hx
@@ -9,4 +9,5 @@ class MacroData
 
     public static macro function getUSR()
         return macro  $v{#if windows Sys.environment()["USERNAME"] #elseif (linux || macos) return Sys.environment()["USER"] #end};
+    #end
 }
