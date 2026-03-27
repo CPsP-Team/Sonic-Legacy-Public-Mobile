b793ad1df27205e8eead654a43c546a898afa46a
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2026-03-27 14:23:02 +0200

    update source/meta/MacroData.hx

--- source/meta/MacroData.hx
+++ source/meta/MacroData.hx
@@ -2,13 +2,11 @@ package meta;
 
 class MacroData
 {
+  var wa;
   #if desktop
     public static macro function getDate()
         return macro  $v{Date.now().toString()};
 
     public static macro function getUSR()
         return macro  $v{#if windows Sys.environment()["USERNAME"] #elseif (linux || macos) return Sys.environment()["USER"] #end};
-  #else
-  trace("Get a fucking pc man, it's not that deep.");
-  #end
 }
